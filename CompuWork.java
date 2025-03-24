92c592926705e7cc0a9110e2003ec32602193b18
yefer54 <villotapantojay@gmail.com>
2025-03-24 10:02:42 -0500

    Add files via upload

---
+++ CompuWork.java
@@ -0,0 +1,16 @@
+package compuwork;
+
+public class CompuWork {
+
+    public static void main(String[] args) {
+        
+        Empleado E1 = new Empleado("Andres Felipe", 25, 1088536728);
+        Empleado E2 = new Empleado("Miguel Angel", 34, 1088564352);
+        Empleado E3 = new Empleado("Maria Elena", 27, 1088635427);
+        System.out.println(E1.toString());
+        System.out.println(E2.toString());
+        System.out.println(E3.toString());
+
+    }
+    
+}
